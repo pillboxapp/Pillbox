01466f3899c54a309879ce733a9e87e4c41adef0
Zach <zach@truckmovers.com>
2017-11-07 16:02:19 -0600

    added spinner to calender view toolbar

--- app/src/main/java/com/pillbox/CalendarActivity.java
+++ app/src/main/java/com/pillbox/CalendarActivity.java
@@ -5,13 +5,21 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -22,6 +30,13 @@ public class CalendarActivity extends AppCompatActivity {
         Intent intent = getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        Toolbar toolBar = findViewById(R.id.calendar_toolbar);
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        addItemsToSpinner();
+
         widget = (MaterialCalendarView) findViewById(R.id.calendarView);
         widget.addDecorator(new HighlightWeekendsDecorator());
         widget.setOnDateChangedListener(new OnDateSelectedListener() {
@@ -35,6 +50,46 @@ public class CalendarActivity extends AppCompatActivity {
                 toast.show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void addItemsToSpinner(){
+        final List<String> list = new ArrayList<String>();
+        list.add("Overview");
+        list.add("pill 1");
+        list.add("pill 2");
+        list.add("pill 3");
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_nav);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Reload calendar for selected medication
+                Context context = getApplicationContext();
+                CharSequence text = list.get(position);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
 
 }
