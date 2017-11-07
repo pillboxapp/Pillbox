00b2b42044e88bb63c5ed3d722eab222310b7d43
Zach <zach@truckmovers.com>
2017-11-06 20:26:16 -0600

    Added date decorators to calendar

--- app/src/main/java/com/pillbox/CalendarActivity.java
+++ app/src/main/java/com/pillbox/CalendarActivity.java
@@ -1,15 +1,40 @@
 package com.pillbox;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    MaterialCalendarView widget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        widget = (MaterialCalendarView) findViewById(R.id.calendarView);
+        widget.addDecorator(new HighlightWeekendsDecorator());
+        widget.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Context context = getApplicationContext();
+                CharSequence text = date.toString();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
+
 }
